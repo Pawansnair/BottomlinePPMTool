88ea627c80f45efcb54922cbc6b526cf4980720e
Nair <Pawan.Nair@bottomline.com>
2020-09-16 17:07:05 +0530

    first commit

---
+++ ppmtool/src/test/java/com/bottomline/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottomline.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
